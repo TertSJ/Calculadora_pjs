d8d47f6479ac32ebad66050a4cb74beb23653929
TertS <tertuliano.s.j@gmail.com>
2025-08-27 21:05:16 -0300

    new1

--- src/main/java/org/model/Operacao.java
+++ src/main/java/org/model/Operacao.java
@@ -1,5 +1,5 @@
 package org.model;
 
 public interface Operacao {
-    public int calc (int a, int b);
+    public int calc(int a, int b);
 }
--- src/main/java/org/view/Menu.java
+++ src/main/java/org/view/Menu.java
@@ -1,5 +1,6 @@
 package org.view;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -15,10 +16,12 @@ public class Menu {
 
     public static void show(){
         Scanner s = new Scanner(System.in);
-        s.nextLine();
-        Reflections reflection = new Reflections("org.model");
-        int opcao;
 
+        Reflections reflection = new Reflections("org.model");
+        int opcao = 0;
+        int a = 0 , b = 0 ;
+
+        Class<? extends Operacao> atualOp;
 
         Set<Class <? extends Operacao>> classeOperacoes = reflection.getSubTypesOf(Operacao.class);
         List<Class <? extends Operacao>> operacoesLista = new ArrayList<>(classeOperacoes);
@@ -27,6 +30,27 @@ public class Menu {
         for (int i = 0; i < operacoesLista.size(); i++){
             System.out.println((i + 1) + " - " + operacoesLista.get(i).getSimpleName());
         }
+        System.out.println("0 - Sair");
+        System.out.println("Digite uma opção :");
+        opcao = s.nextInt();
+
+        if (opcao<= operacoesLista.size()){
+            System.out.print("Digite o primeiro número : ");
+            a = s.nextInt();
+            System.out.print("Digite o segundo número : ");
+            b = s.nextInt();
+
+            atualOp = operacoesLista.get(opcao);
+            Object instancia = atualOp.getDeclaredConstructor().newInstance();
+
+            // Procura o método "executar"
+            Method metodo = atualOp.getMethod("calc");
+
+            System.out.println(metodo.invoke(instancia));
+        }
+
+
+
 
 
     }
